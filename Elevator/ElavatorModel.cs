6891acdd8bde7c20abcc4247592f5026d2d9d2df
MatheusBarbosa0798 <matheusbarbosa0798@gmail.com>
2021-08-02 20:45:22 -0300

    v1

---
+++ Elevator/ElavatorModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Elevator
+{
+    public class ElavatorModel
+    {
+        private const int GroundFloor = 0;
+
+        private int TotalFloors { get; set; }
+        private int CurrentFloor { get; set; }
+        private int TotalCapacity { get; set; }
+        private int CurrentCapacity { get; set; }
+
+        public ElavatorModel(int totalCapacity, int totalFloors)
+        {
+            CurrentFloor = 0;
+            CurrentCapacity = 0;
+            TotalCapacity = totalCapacity;
+            TotalFloors = totalFloors;
+        }
+
+        public void Enter()
+        {
+            if (CurrentCapacity < TotalCapacity)
+            {
+                CurrentCapacity++;
+                Console.WriteLine("Entrou!\n" + CurrentCapacity + " pessoas no elevador.");
+            }
+            else
+            {
+                Console.WriteLine("O elevador está cheio!");
+            }
+        }
+
+        public void Exit()
+        {
+            if (CurrentCapacity > 0)
+            {
+                CurrentCapacity--;
+                Console.WriteLine("Saiu!\n" + CurrentCapacity + " pessoas no elevador.");
+            }
+            else
+            {
+                Console.WriteLine("O elevador já está vazio!");
+            }
+        }
+
+        public void Up()
+        {
+            if (CurrentFloor < TotalFloors)
+            {
+                CurrentFloor++;
+                Console.WriteLine("Subindo!\n" + CurrentFloor + "° Andar");
+            }
+            else
+            {
+                Console.WriteLine("Você já está no último andar!");
+            }
+        }
+
+        public void Down()
+        {
+            if (CurrentFloor > 0)
+            {
+                CurrentFloor--;
+                if (CurrentFloor == ElavatorModel.GroundFloor)
+                {
+                    Console.WriteLine("Descendo!\nTérreo");
+                }
+                else
+                {
+                    Console.WriteLine("Descendo!\n" + CurrentFloor + "° Andar");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Você já está no térreo!");
+            }
+        }
+    }
+}
---
+++ Elevator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Elevator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int TotalFloors = 10;
+            int TotalCapacity = 15;
+            int Option;
+
+            ElavatorModel elevatorModel = new ElavatorModel(TotalCapacity, TotalFloors); //Construtor
+
+            while (true)
+            {
+                Options();
+                Option = Int32.Parse(Console.ReadLine());
+                switch (Option)
+                {
+                    case 0:
+                        elevatorModel.Enter();
+                        break;
+                    case 1:
+                        elevatorModel.Exit();
+                        break;
+                    case 2:
+                        elevatorModel.Up();
+                        break;
+                    case 3:
+                        elevatorModel.Down();
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        break;
+                }
+            }
+        }
+
+        public static void Options()
+        {
+            Console.WriteLine("Aperte:\n");
+            Console.WriteLine("0 - Entrar\n");
+            Console.WriteLine("1 - Sair\n");
+            Console.WriteLine("2 - Subir\n");
+            Console.WriteLine("3 - Descer\n");
+        }
+    }
+}
